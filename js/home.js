d149c2148c69c1aa50729718d08e99a4a38754cc
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-11-10 19:43:21 -0300

    Open feature added

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+const menuWrapper = document.querySelector('.menu-wrapper')
 const menuBurguer = document.querySelector('.menu-btn')
 
 menuBurguer.addEventListener('click', openMenu)
@@ -7,9 +8,11 @@ let menuToggle = false;
 function openMenu(){
     if(!menuToggle){
         menuBurguer.classList.add('open')
+        menuWrapper.style.height = "100vh"
         menuToggle = true;
     }else{
         menuBurguer.classList.remove('open')
+        menuWrapper.style.height = "0%"
         menuToggle = false;
     }
 }
